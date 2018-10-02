3cb367b87883562c20b6ae46bde5aa0cce237ec2
martinKindall <martin@rflex.cl>
2018-10-02 11:18:44 -0300

    Se agregan comentarios

--- tarea1/Afnd.py
+++ tarea1/Afnd.py
@@ -13,32 +13,36 @@ class Afnd:
 
 
 	@staticmethod
-	def _actualizarContador(cont):
-		cont += 1
-		c1 = cont
-		cont += 1
-		c2 = cont
+	def _actualizarContador(indice):
+		indice += 1
+		id1 = indice
+		indice += 1
+		id2 = indice
 
-		return (cont, c1, c2)
+		return (indice, id1, id2)
 
 
+	# _erToAfnd: string -> None
+	# convierte una expresion regular a un automata
+	# no deterministico usando Thompson!
+	# efecto: modifica el afnd		
 	def _erToAfnd(self, regExp):
 
 		indice = -1
-		c1 = 0
-		c2 = 0
+		id1 = 0
+		id2 = 0
 
 		for char in regExp[::-1]:
 			if char in self.alfabeto:
-				indice, c1, c2 = self._actualizarContador(indice)
+				indice, id1, id2 = self._actualizarContador(indice)
 
 				afnd1 = {}
 				afnd2 = {}
-				afnd1[char] = c2
+				afnd1[char] = id2
 				self.deltas.append(afnd1)
 				self.deltas.append(afnd2)
 
-				self.arcos.apilar([c1, c2])
+				self.arcos.apilar([id1, id2])
 
 			elif char == '.':
 				arco21, arco22 = self.arcos.desapilar()
@@ -53,36 +57,36 @@ class Afnd:
 					self.final = arco22
 
 			elif char == '|':
-				indice, c1, c2 = self._actualizarContador(indice)
+				indice, id1, id2 = self._actualizarContador(indice)
 				arco21, arco22 = self.arcos.desapilar()
 				arco11, arco12 = self.arcos.desapilar()
-				self.arcos.apilar([c1, c2])
+				self.arcos.apilar([id1, id2])
 
 				self.deltas.append({})
 				self.deltas.append({})
-				self.deltas[c1]['€'] = (arco11, arco21)
-				self.deltas[arco12]['€'] = c2
-				self.deltas[arco22]['€'] = c2
+				self.deltas[id1]['€'] = (arco11, arco21)
+				self.deltas[arco12]['€'] = id2
+				self.deltas[arco22]['€'] = id2
 
 				if self.inicio == arco21 or self.inicio == arco11:
-					self.inicio = c1
+					self.inicio = id1
 				if self.final == arco22 or self.final == arco12:
-					self.final = c2
+					self.final = id2
 
 			elif char == '*':
-				indice, c1, c2 = self._actualizarContador(indice)
+				indice, id1, id2 = self._actualizarContador(indice)
 				arco1, arco2 = self.arcos.desapilar()
-				self.arcos.apilar([c1, c2])
+				self.arcos.apilar([id1, id2])
 
 				self.deltas.append({})
 				self.deltas.append({})
-				self.deltas[c1]['€'] = (arco1, c2)
-				self.deltas[arco2]['€'] = (arco1, c2)
+				self.deltas[id1]['€'] = (arco1, id2)
+				self.deltas[arco2]['€'] = (arco1, id2)
 
 				if self.inicio == arco1:
-					self.inicio = c1
+					self.inicio = id1
 				if self.final == arco2:
-					self.final = c2
+					self.final = id2
 			
 			else:
 				print("La expresión contiene un caracter fuera del alfabeto")
