483145c11069e515b8f2581346841f120e91f75a
martinKindall <martin@rflex.cl>
2018-10-04 21:56:05 -0300

    Primer intento que busca matches con terminaciones finales

--- tarea1/Afd.py
+++ tarea1/Afd.py
@@ -5,11 +5,22 @@ class Afd:
 		self.inicio = 0
 		self.estadosFinales = estadosFinales
 
-	def aceptarCadena(self, cadena):
-		estadoActual = self.inicio
 
-		for char in cadena:
+	def aceptarCadena(self, cadena):
+		terminos = self.buscarPatrones(cadena)
+
+		return len(terminos) > 0 and (terminos.pop() + 1) == len(cadena)
+
+
+	def buscarPatrones(self, cadena):
+		estadoActual = self.inicio
+		terminos = []
+
+		for idx, char in enumerate(cadena):
 			if char in self.delta[estadoActual]:
 				estadoActual = self.delta[estadoActual][char]
 
-		return estadoActual in self.estadosFinales
+				if estadoActual in self.estadosFinales:
+					terminos.append(idx)
+
+		return terminos
--- tarea1/tarea1.py
+++ tarea1/tarea1.py
@@ -2,25 +2,36 @@ from Afnd import Afnd
 
 import pdb
 
-
 Afnd.tests()
 
+
+def principal(accion, automata):
+	continuar = "s"
+	while continuar == "s":
+		cadena = input("Ingrese cadena: ")
+		mensaje = accion(cadena, automata)
+		print(mensaje)
+		continuar = input("Desea continuar? (s/n): ")
+
+
+def buscarMaches(cadena, automata):
+		return automata.buscarPatrones(cadena)
+
+def buscarCadenas(cadena, automata):
+		return automata.aceptarCadena(cadena)
+
 # regExp = "|..baba"
-regExp = "*|.ab..aba"
+regExp = "|a..bab"
 # regExp = "|ab"
 
 # alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 \n"
 alfabeto = "ab"
 afnd1 = Afnd.fromRegExpToAfnd(regExp, alfabeto, [])
-# afnd1.addLoopsTexto()
+afnd1.addLoopsTexto()
 afd1 = afnd1.convertToAfd()
 
-continuar = "s"
+principal(buscarMaches, afd1)
 
-while continuar == "s":
-	cadena = input("Ingrese cadena: ")
-	respuesta = "El automata acepto la cadena" if afd1.aceptarCadena(cadena) else "Cadena no reconocida"
-	print(respuesta)
-	continuar = input("Desea continuar? (s/n): ")
+
 
 # pdb.set_trace();True
