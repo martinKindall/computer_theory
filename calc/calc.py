c84882d7f0902470ef96716a78934eea6005f32b
martinKindall <martin@rflex.cl>
2018-12-04 19:29:13 -0300

    Se agregan reglas de gramatica

--- calc/calc.py
+++ calc/calc.py
@@ -50,13 +50,47 @@ lexer = lex.lex()
 
 lexer.input("abc = 123.456")
 
+
+def p_calc(p):
+	'''
+	calc : expression
+	     | empty
+	'''
+	print(p[1])
+
+
+def p_expression(p):
+	'''
+	expression : expression MULTIPLY expression
+			   | expression DIVIDE expression
+			   | expression PLUS expression
+			   | expression MINUS expression
+	'''
+	p[0] = (p[2], p[1], p[3])
+
+
+
+def p_expression_int_float(p):
+	'''
+	expression : INT
+			   | FLOAT
+	'''
+	p[0] = p[1]
+
+
+def p_empty(p):
+	'''
+	empty :
+	'''
+	p[0] = None
+
+
+parser = yacc.yacc()
+
 while True:
-	tok = lexer.token()
-
-	if not tok:
+	try:
+		s = input('')
+	except EOFError:
 		break
-	print(tok)
 
-
-
-
+	parser.parse(s)
